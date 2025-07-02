add7ac97bb65621b786937ae90f03dd94a211d04
lichyot <douglasferrs@gmail.com>
2025-07-02 16:44:33 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade: Login', () => {
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('eva@test.com')
+        cy.get('#password').type('Eva.2015')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, eva (não é eva? Sair)')
+    })
+})
